545feebd6c60e67a9d992e38d6ead60265132acc
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-04-11 16:17:07 +0800

    weapon blocking

--- src/main/java/dev/xkmc/jerotes_golems/events/JGEventHandler.java
+++ src/main/java/dev/xkmc/jerotes_golems/events/JGEventHandler.java
@@ -1,9 +1,13 @@
 package dev.xkmc.jerotes_golems.events;
 
 import com.jerotes.jerotes.forge.JerotesMerorDamageEvent;
+import com.jerotes.jerotes.item.Interface.ItemTwoHanded;
 import dev.xkmc.jerotes_golems.init.JerotesGolems;
 import dev.xkmc.jerotes_golems.init.reg.JGModifiers;
 import dev.xkmc.modulargolems.content.entity.common.AbstractGolemEntity;
+import dev.xkmc.modulargolems.content.entity.humanoid.HumanoidGolemEntity;
+import net.minecraft.world.InteractionHand;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -15,6 +19,19 @@ public class JGEventHandler {
 		if (event.getEntity() instanceof AbstractGolemEntity<?, ?> golem) {
 			if (golem.getModifiers().containsKey(JGModifiers.MEROR_ATK.get())) {
 				event.setMerorDamage(true);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void humanoidBlock(LivingEvent.LivingTickEvent event) {
+		if (event.getEntity() instanceof HumanoidGolemEntity golem) {
+			if (golem.getMainHandItem().getItem() instanceof ItemTwoHanded item && item.canBlock()) {
+				if (golem.getOffhandItem().isEmpty()) {
+					if (!golem.isUsingItem()) {
+						golem.startUsingItem(InteractionHand.MAIN_HAND);
+					}
+				}
 			}
 		}
 	}
